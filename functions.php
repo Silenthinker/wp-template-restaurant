4e491fa59cc97d890ca75c0440294b10b2bc9ca3
Silenthinker <sparklethinker@gmail.com>
2017-04-01 22:57:48 +0200

    fix loading-twice problem

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 	function add_theme_scripts() {
-	    wp_enqueue_style( 'style', get_stylesheet_uri()+'/assets/css/style.css' );
+	    wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css' );
 	    wp_enqueue_script( 'home', get_template_directory_uri() . '/assets/js/home.js', array ( 'jquery' ), 1.1, true);
 	    wp_enqueue_script( 'menu', get_template_directory_uri() . '/assets/js/menu.js', array ( 'jquery' ), 1.1, true);
 	}
