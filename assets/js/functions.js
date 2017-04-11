6b04597701577182c09360d98b27b8f3216feeaf
Silenthinker <sparklethinker@gmail.com>
2017-04-11 09:43:14 +0200

    wrap functions in functions.js in document ready

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -37,37 +37,38 @@ var state = {
         name: "detail"
 };
 
-$(".detail-event").hide();
-jQuery( document ).on( 'click', '.thumbnail, h3.clickable', function() {
-    var height = $("section.event").css("height");
-     // do AJAX in DETAIL-EVENT Block
-    var post_id = this.id;
-    var call_type = 2;
-    var str = '&post_id='+post_id+"&call_type="+call_type+'&action=ajax_more_posts'; //'&pageNumber=' + pageNumber + '&ppp=' + ppp + '&action=ajax_more_posts';
-    $.ajax({
-        type: "post",
-        dataType: "html",
-        url: ajaxmoreposts.ajaxurl,
-        data: str,
-        success: function(data){
-            $(".past-events").css("display","none");
-            $(".upcoming-events").css("display","none");
-            $(".see-button").css("display","none");
-            $(".detail-event").css({"display":"block", "height":height});
-            $(".detail-event").html(data);
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-            $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
-        }
+$(document).ready(function(){
+    $(".detail-event").hide();
+    jQuery( document ).on( 'click', '.thumbnail, h3.clickable', function() {
+        var height = $("section.event").css("height");
+         // do AJAX in DETAIL-EVENT Block
+        var post_id = this.id;
+        var call_type = 2;
+        var str = '&post_id='+post_id+"&call_type="+call_type+'&action=ajax_more_posts'; //'&pageNumber=' + pageNumber + '&ppp=' + ppp + '&action=ajax_more_posts';
+        $.ajax({
+            type: "post",
+            dataType: "html",
+            url: ajaxmoreposts.ajaxurl,
+            data: str,
+            success: function(data){
+                $(".past-events").css("display","none");
+                $(".upcoming-events").css("display","none");
+                $(".see-button").css("display","none");
+                $(".detail-event").css({"display":"block", "height":height});
+                $(".detail-event").html(data);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+            }
+        });
+        window.history.pushState(state, "", "./event-details");
     });
-    window.history.pushState(state, "", "./event-details");
-});
-window.addEventListener("popstate", function(e) {
-        var state = window.history.state;
-        
-        $(".detail-event").css("display","none");
-        $(".past-events").show();
-        $(".upcoming-events").show();
-        $(".see-button").show();
-});
-
+    window.addEventListener("popstate", function(e) {
+            var state = window.history.state;
+            
+            $(".detail-event").css("display","none");
+            $(".past-events").show();
+            $(".upcoming-events").show();
+            $(".see-button").show();
+    });
+};
